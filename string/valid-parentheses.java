706319a661ca7d044de71fa07f3e3908303006b8
JyzPoohs <129666575+JyzPoohs@users.noreply.github.com>
2026-07-06 17:32:32 +0800

    Synchronize existing submission history (1/1)

---
+++ string/valid-parentheses.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+class Solution {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                
+                if (c == ')') {
+                    if (stack.peek() == '(') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (c == '}') {
+                    if (stack.peek() == '{') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (c == ']') {
+                    if (stack.peek() == '[') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
