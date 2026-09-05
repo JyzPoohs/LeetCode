31520eac12490abb2b9b04a50061ccd0f303c69d
JyzPoohs <129666575+JyzPoohs@users.noreply.github.com>
2026-09-06 01:45:34 +0800

    LC-search-insert-position [Runtime: N/A; Memory: N/A]

--- array/search-insert-position.java
+++ array/search-insert-position.java
@@ -1,29 +1,14 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int mid = nums[nums.length/2];
-        int start = 0;
-        int end = nums.length;
-        int output = 0;
+        int left = 0; int right = nums.length - 1;
 
-        if (target == mid) {
-            return nums.length/2;
-        } else if(target > nums[nums.length - 1]) {
-            return nums.length;
-        } else if(target > mid) {
-            start = nums.length/2;
-            end = nums.length;
-        } else if (target < mid) {
-            start = 0;
-            end = nums.length/2;
-        } 
-
-        for(int i = start; i < end; i++) {
-            if(target > nums[i] &&  target <= nums[i+1]) {
-                output = i + 1;
-                break;
-            }
+        while(left <= right) {
+            int mid = left + (right - left) / 2; 
+            if(nums[mid] == target) return mid;
+            if(nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
         }
 
-        return output;
+        return left;
     }
 }
